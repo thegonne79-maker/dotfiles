ede7ce57f2af82dd38aeb7a87236027678332108
thegonne79-maker <thegonne79@gmail.com>
2026-04-28 07:42:58 -0600

    Disable home-manager input in flake.nix
    Comment out home-manager input in flake.nix sempi said no

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    # home-manager.url = "github:nix-community/home-manager";
     nix-citizen.url = "github:LovingMelody/nix-citizen";
     nix-citizen.inputs.nix-gaming.follows = "nix-gaming";
     nix-gaming.url = "github:fufexan/nix-gaming";
